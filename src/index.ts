526d0ec6b4407247ff28b4a689f059c25e96d901
NAYARA CRISTINE BRAGA PEREIRA <57842812+nayahcris@users.noreply.github.com>
2021-05-01 16:39:36 -0300

    #HRK

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,9 @@ import routes from "./routes";
 const app = express()
 createConnection()
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(routes)
+//app.use(cors())
+//app.use(bodyParser.json())
+//app.use(routes)
 
 
 app.listen(3333)
