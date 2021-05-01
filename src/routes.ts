554ca500186f8e0d799de4297a56d38ab618f779
NAYARA CRISTINE BRAGA PEREIRA <57842812+nayahcris@users.noreply.github.com>
2021-05-01 16:36:27 -0300

    #HRK

--- src/routes.ts
+++ src/routes.ts
@@ -13,7 +13,7 @@ import { getRevistas, getRevista } from './controller/RevistaController'
 
 const routes = Router()
 
-
+routes.get('/', getContos)
 //ROTAS DE TAREFAS QUE PRECISO RETIRAR DEPOIS
 //routes.get('/tasks', getTasks)
 //routes.get('/tasks/:id', getTask)
