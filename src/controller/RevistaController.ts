8670d0255501ad07a28b279b4f750dc095c702ba
NAYARA CRISTINE BRAGA PEREIRA <57842812+nayahcris@users.noreply.github.com>
2021-06-06 14:01:02 -0300

    #ajuste na controller

--- src/controller/RevistaController.ts
+++ src/controller/RevistaController.ts
@@ -15,3 +15,33 @@ export const getRevista = async (request: Request, response: Response) => {
     const revista = await getRepository(Revista).findOne(_idRevista)
     return response.json(revista)
 };
+
+//ATUALIZA UMA REVISTA
+export const updateRevista = async(request: Request, response: Response) => {
+    const revistaRepository = getRepository(Revista);
+    const { _idRevista } = request.params
+    const revista = await revistaRepository.findOne(_idRevista, {relations: ["_responsavel"]})
+    const revistaatt = revistaRepository.merge(revista, request.body)
+
+    return response.json(revistaatt)
+};
+
+
+//SALVA REVISTA
+export const saveRevista = async(request: Request, response: Response) => {
+    const revista = await getRepository(Revista).save(request.body)
+    return response.json(revista);
+};
+
+//DELETA REVISTA
+export const removeRevista = async (request: Request, response: Response) => {
+    const { _idRevista } = request.params
+    const revista = await getRepository(Revista).delete(_idRevista)
+
+    if (revista.affected === 1){
+        const revistaRemovido = await getRepository(Revista).findOne(_idRevista, {relations: ["_responsavel"]})
+        return response.status(200)
+    }
+
+    return response.status(404)
+};
